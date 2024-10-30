b30ccdc0e599314b697638d9f33e3d3c66def394
Aki <x.jaketem@gmail.com>
2024-10-30 16:54:20 +0000

    Add files via upload

---
+++ Library-System-main/LibrarySystem.exe.py
@@ -0,0 +1,24 @@
+import tkinter # makes sure to import the GUI to start with
+from random import randint # imports randomness
+try: # makes sure the program can still run with no errors
+    
+    for Multiplier in range(45,80):
+        NewKey = 926746727 * Multiplier
+        
+
+    with open("Library-System-main\README\CyrnfrQbagQryrgrZrjnnnnn.txt","r") as Verification: # makes sure info has not be deleted
+        CollectedData = Verification.readlines()
+        Error = True
+        for line in CollectedData:
+            if "Key" in line:
+                Error = False
+
+    #NewWindow = tkinter.Tk()
+    #NewWindow.title("Main Menu")
+    #BoundingFrame = tkinter.Frame(width=10,height=10) 
+    #BoundingFrame.pack()
+
+except:
+    print("Failed to Run")
+
+
